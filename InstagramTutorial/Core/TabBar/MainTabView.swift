df81b5bb66db5b1f72cc69cb28313f794ccdba0a
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-07-10 15:59:51 +0300

    Tab Bar Setup

---
+++ InstagramTutorial/Core/TabBar/MainTabView.swift
@@ -0,0 +1,42 @@
+//
+//  MainTabView.swift
+//  InstagramTutorial
+//
+//  Created by Sasha Maksyutenko on 10.07.2023.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView{
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .accentColor(.black)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
