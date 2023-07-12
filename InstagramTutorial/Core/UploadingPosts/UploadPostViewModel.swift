e8b2c3121411d41dd236619249aaa7803ebc4a89
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-07-12 21:11:00 +0300

    Upload Post View

---
+++ InstagramTutorial/Core/UploadingPosts/UploadPostViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  UploadPostViewModel.swift
+//  InstagramTutorial
+//
+//  Created by Sasha Maksyutenko on 12.07.2023.
+//
+
+import Foundation
+import PhotosUI
+import SwiftUI
+@MainActor
+class UploadPostViewModel:ObservableObject{
+    @Published var selectedImage:PhotosPickerItem?{
+        didSet{ Task {await loadImage(fromItem: selectedImage)}}
+    }
+    @Published var postImage:Image?
+    func loadImage(fromItem item:PhotosPickerItem?)async{
+        guard let item=item else {return}
+        guard let data=try?await item.loadTransferable(type: Data.self) else {return}
+        guard let uiImage=UIImage(data:data) else {return}
+        self.postImage=Image(uiImage:uiImage)
+    }
+}
