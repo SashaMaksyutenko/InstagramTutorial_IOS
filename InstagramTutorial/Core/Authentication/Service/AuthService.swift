ff34ee69bf969baaa7695cca33d8459383bbc231
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-07-14 00:38:48 +0300

    Fetch  User Data

--- InstagramTutorial/Core/Authentication/Service/AuthService.swift
+++ InstagramTutorial/Core/Authentication/Service/AuthService.swift
@@ -11,15 +11,17 @@ import FirebaseFirestoreSwift
 import Firebase
 class AuthService{
     @Published var userSession:FirebaseAuth.User?
+    @Published var currentUser:User?
     static let shared=AuthService()
     init(){
-        self.userSession=Auth.auth().currentUser
+        Task{try await loadUserData()}
     }
     @MainActor
     func login(withEmail email:String, password:String)async throws{
         do{
             let result=try await Auth.auth().signIn(withEmail: email, password: password)
             self.userSession=result.user
+            try await loadUserData()
         } catch{
             print("Debug: Failed to log in with error \(error.localizedDescription)")
         }
@@ -34,15 +36,21 @@ class AuthService{
             print("Debug: Failed to register User with error \(error.localizedDescription)")
         }
     }
+    @MainActor
     func loadUserData()async throws{
-        
+        self.userSession=Auth.auth().currentUser
+        guard let currentUid=userSession?.uid else{return}
+        let snapshot=try await Firestore.firestore().collection("users").document(currentUid).getDocument()
+        self.currentUser = try? snapshot.data(as: User.self)
     }
     func signOut(){
         try?Auth.auth().signOut()
         self.userSession=nil
+        self.currentUser=nil
     }
     private func uploadUserData(uid:String,username:String,email:String)async{
         let user=User(id: uid, username: username, email: email)
+        self.currentUser=user
         guard let encodedUser=try? Firestore.Encoder().encode(user) else{
             return
         }
--- InstagramTutorial/Core/Root/ViewModel/ContentViewModel.swift
+++ InstagramTutorial/Core/Root/ViewModel/ContentViewModel.swift
@@ -12,12 +12,17 @@ class ContentViewModel:ObservableObject{
     private let service=AuthService.shared
     @Published var userSession:FirebaseAuth.User?
     @Published var cancellables=Set<AnyCancellable>()
+    @Published var currentUser:User?
     init(){
         setupSubscribers()
     }
     func setupSubscribers(){
         service.$userSession.sink{ [weak self] userSession in
             self?.userSession=userSession
+        }
+        .store(in: &cancellables)
+        service.$currentUser.sink{ [weak self] currentUser in
+            self?.currentUser=currentUser
         }
         .store(in: &cancellables)
     }
