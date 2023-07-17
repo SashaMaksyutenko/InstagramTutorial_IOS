63822450f67ea30265b77640d1c4a735e90d3b7d
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-07-18 00:18:44 +0300

    Fetch User Posts

--- InstagramTutorial/Core/Profile/View/CurrentUserProfileView.swift
+++ InstagramTutorial/Core/Profile/View/CurrentUserProfileView.swift
@@ -9,9 +9,6 @@ import SwiftUI
 
 struct CurrentUserProfileView: View {
     let user:User
-    var posts:[Post]{
-    return Post.MOCK_POSTS.filter({$0.user?.username==user.username})
-}
     var body: some View {
         NavigationStack{
             ScrollView
@@ -19,7 +16,7 @@ struct CurrentUserProfileView: View {
                 // header
                 ProfileHeaderView(user: user)
                 // post grid view
-                PostGridView(posts: posts)
+                PostGridView(user: user)
             }
             .navigationTitle("Profile")
             .navigationBarTitleDisplayMode(.inline)
